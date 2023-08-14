1e6b98f84dfe11e64f58d201bf5c658d7a03000e
zakibourmada <119577305+zakibourmada@users.noreply.github.com>
2023-08-14 15:10:15 +0000

    new file:   src/logging.cpp

---
+++ src/logging.cpp
@@ -0,0 +1,18 @@
+#include "logging.h"
+
+// Implement functions for logging here
+
+// Example function to log flight events
+void logFlightEvent(const FlightEvent& event) {
+    // Implement code to log flight events, sensor readings, and system status
+}
+
+// Example function to log sensor data
+void logSensorData(const SensorData& data) {
+    // Implement code to log sensor readings
+}
+
+// Example function to log system status
+void logSystemStatus(const SystemStatus& status) {
+    // Implement code to log system status
+}
