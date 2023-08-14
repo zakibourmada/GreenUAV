12b4e27e68b9d71828d71ddb045736231e1aa082
zakibourmada <119577305+zakibourmada@users.noreply.github.com>
2023-08-14 14:37:42 +0000

    new file:   src/state_estimator.cpp

---
+++ src/state_estimator.cpp
@@ -0,0 +1,11 @@
+#include "state_estimator.h"
+
+// Implement functions for state estimation here
+
+// Example function to estimate UAV state
+UAVState estimateUAVState(const SensorData& sensorData) {
+    // Implement code to estimate UAV state based on sensor inputs
+    UAVState uavState;
+    // Perform state estimation and populate 'uavState'
+    return uavState;
+}
