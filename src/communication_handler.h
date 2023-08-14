c1d16d65300e0e06047940836525cf467828f70d
zakibourmada <119577305+zakibourmada@users.noreply.github.com>
2023-08-14 15:01:16 +0000

    new file:   src/communication_handler.h

---
+++ src/communication_handler.h
@@ -0,0 +1,17 @@
+#ifndef COMMUNICATION_HANDLER_H
+#define COMMUNICATION_HANDLER_H
+
+// Define your data structures here
+struct CommunicationData {
+    // Define the structure of communication data
+};
+
+struct Command {
+    // Define the structure of a command
+};
+
+// Declare functions for communication handling
+void handleCommunication(const CommunicationData& data);
+Command receiveCommand();
+
+#endif // COMMUNICATION_HANDLER_H
