f480bddaebc28435f52dbee532527251e2238c24
zakibourmada <119577305+zakibourmada@users.noreply.github.com>
2023-08-14 19:33:01 +0000

    new file:   src/data_storage.cpp

---
+++ src/data_storage.cpp
@@ -0,0 +1,29 @@
+#include "data_storage.h"
+
+// Implement functions for data storage here
+
+// Example function to store configuration data
+void storeConfigurationData(const ConfigurationData& data) {
+    // Implement code to store configuration data
+}
+
+// Example function to retrieve configuration data
+ConfigurationData retrieveConfigurationData() {
+    // Implement code to retrieve configuration data
+    ConfigurationData data;
+    // Populate 'data' with retrieved configuration data
+    return data;
+}
+
+// Example function to store waypoints
+void storeWaypoints(const std::vector<Waypoint>& waypoints) {
+    // Implement code to store waypoints
+}
+
+// Example function to retrieve waypoints
+std::vector<Waypoint> retrieveWaypoints() {
+    // Implement code to retrieve waypoints
+    std::vector<Waypoint> waypoints;
+    // Populate 'waypoints' with retrieved waypoints
+    return waypoints;
+}
