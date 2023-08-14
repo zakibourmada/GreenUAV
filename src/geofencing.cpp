e1be36f9dc9f2411817abf30ca1b17b054548dc2
zakibourmada <119577305+zakibourmada@users.noreply.github.com>
2023-08-14 14:49:57 +0000

    new file:   src/geofencing.cpp

---
+++ src/geofencing.cpp
@@ -0,0 +1,9 @@
+#include "geofencing.h"
+
+// Implement functions for geofencing here
+
+// Example function to check if UAV is within boundaries
+bool isWithinGeofence(const UAVPosition& position, const GeofenceBounds& bounds) {
+    // Implement code to determine if the UAV is within defined boundaries
+    // Return true if within, false otherwise
+}
